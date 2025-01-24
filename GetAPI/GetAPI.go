ad125a3ed9076a430c8420555d14ce03598569bc
zoom26042604 <nathan.ferre@ynov.com>
2025-01-24 16:39:46 +0100

    fix(all): merge developp

--- GetAPI/GetAPI.go
+++ GetAPI/GetAPI.go
@@ -36,3 +36,52 @@ func (c *APIClient) Fetch(endpoint string, target interface{}) error {
 
 	return json.NewDecoder(resp.Body).Decode(target)
 }
+
+func GetCombinedData(artists []ArtistAPI, locations LocationsAPI, dates DatesAPI, relations RelationAPI) []CombinedData {
+	var combinedData []CombinedData
+	for _, artist := range artists {
+		combinedData = append(combinedData, CombinedData{
+			Artist:    artist,
+			Locations: locations,
+			Dates:     dates,
+			Relations: relations,
+		})
+	}
+	return combinedData
+}
+
+func GetCombinedDataByID(artists []ArtistAPI, locations []LocationsAPI, dates []DatesAPI, relations []RelationAPI, id int) (CombinedData, error) {
+	var combinedData CombinedData
+	for _, artist := range artists {
+		if artist.ID == id {
+			combinedData.Artist = artist
+			break
+		}
+	}
+	if combinedData.Artist.ID == 0 {
+		return combinedData, fmt.Errorf("artist with ID %d not found", id)
+	}
+
+	for _, location := range locations {
+		if location.ID == id {
+			combinedData.Locations = location
+			break
+		}
+	}
+
+	for _, date := range dates {
+		if date.ID == id {
+			combinedData.Dates = date
+			break
+		}
+	}
+
+	for _, relation := range relations {
+		if relation.ID == id {
+			combinedData.Relations = relation
+			break
+		}
+	}
+
+	return combinedData, nil
+}
